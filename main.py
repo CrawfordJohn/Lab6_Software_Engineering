68aa84b90d753ec711ac99932190713e3773daec
John Crawford <crawfordjohn@ufl.edu>
2023-03-09 15:09:21 -0500

    deleted print statement that was used to check work and added comments

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Lab 6: Software Engineering
 """
 
 
-def print_menu():
+def print_menu(): #function to print menu screen
     print('Menu')
     print('-------------')
     print('1. Encode')
@@ -13,17 +13,17 @@ def print_menu():
     print('')
 
 
-def encode(password):
+def encode(password): #encode 8 digit password by adding 3 to every digit
     encoded_password = ''
     for digit in password:
-        if int(digit) >= 7:
+        if int(digit) >= 7: #only one digit so 7 -> 0 instead of 7 -> 10
             encoded_password += str(int(digit) + 3)[-1]
         else:
             encoded_password += str(int(digit) + 3)
     return encoded_password
 
 
-def decode(encoded_password):
+def decode(encoded_password): #decode encoded password back to original
     decoded_password = ""
     for ch in encoded_password:
         if int(ch) >= 3:
@@ -41,7 +41,6 @@ def main():
         if user_input == '1':
             password = input('Please enter your password to encode: ')
             encoded_password = encode(password)
-            print(encoded_password)
             print('Your password has been encoded and stored! \n')
 
         elif user_input == '2':
